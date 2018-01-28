53136ab7d62a70b4e14243af4e222316632a81e6
Tomohiro <12.26tomo.aoki@gmail.com>
2018-01-28 14:56:25 +0900

    update: 猫の手リストをサーバからデータ取得する

--- js/cathelp.js
+++ js/cathelp.js
@@ -74,6 +74,80 @@ $('#room-search').on('submit', function() {
   return false;
 });
 
+// modal window
 $(document).ready(function(){
   $('.modal').modal();
+});
+
+// ヤベェボタン
+$("#yabeBtn").click(function () {
+    $('#modal1').modal('close');
+    $.ajax({
+        type: 'POST',
+        url: "http://127.0.0.1/" + 'aaa',
+        timeout: 10000,         // タイムアウト(ミリ秒)
+        cache: true,            // キャッシュするかどうか
+        // async: false,        // 同期通信にする : false
+        // data: sendData || null,     // サーバに送信するデータ(name: value)
+        dataType: 'json',       // レスポンスが適切なContentTypeを返していれば自動判別します。
+        beforeSend: function(jqXHR) {       // Ajax通信前処理
+            return true;        // falseを返すと処理を中断
+        }
+    })
+});
+
+// ネこの手リスト
+$(document).ready( function(){
+
+    var j = {
+        "0":{
+            "name" : "aaa",
+            "position" : "bbb",
+            "time" : "10:00-12:00"
+        },
+        "1":{
+            "name" : "aaaa",
+            "position" : "bbb",
+            "time" : "10:00-12:00"
+        }, 
+        "2":{
+            "name" : "aaaaa",
+            "position" : "bbb",
+            "time" : "10:00-12:00"
+        }
+    }
+
+    var base = $("#cat-help tbody");
+    
+    
+    
+    for(let i in j) {
+        var tr = $("<tr>");
+
+        var thName = $("<td>");
+        thName.append(j[i].name);
+
+        var thPosition = $("<td>");
+        thPosition.append(j[i].position); 
+
+        var thTime= $("<td>");
+        thTime.append(j[i].time);
+
+        var thBtn = $("<td>");
+        thBtn.addClass("help-"+j[i].name); // ヘルプの要請ID
+        thBtn.append("<a class=\"waves-effect waves-light btn catHelpBtn\">救援要請</a>");
+
+        tr.append(thName);
+        tr.append(thPosition);
+        tr.append(thTime);
+        tr.append(thBtn);
+
+        base.append(tr);
+        
+    }
+
+});
+
+$(document).on("click", ".catHelpBtn", function (e) {
+    console.log(e.target.parentElement.className);
 });
